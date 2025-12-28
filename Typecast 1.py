7ca95a2b0734e1ada5e6b6a25ab3c0ac7f916e33
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-28 16:01:39 +0530

    Create Typecast 1.py
    works perfectly, changes float type into string and integer type.

---
+++ Typecast 1.py
@@ -0,0 +1,9 @@
+#program to typecast by taking float input from user and coverting them to string and integer type
+
+num=float(input("Enter the 1st float value: "))
+num1=float(input("Enter the 2nd flaot value: "))
+
+print(num,num1)
+print(num+num1)
+print(int(num)+int(num1))
+
