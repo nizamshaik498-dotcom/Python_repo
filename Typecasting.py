31eec745a0676d95aa40913726c5edd0ff0d23a8
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-28 13:30:34 +0530

    Create Typecasting.py
    works perfectly, coverts string values to integer values and performs arthimetic operations

---
+++ Typecasting.py
@@ -0,0 +1,12 @@
+#program to typecast the input numbers given by the user 
+
+A=(input("Enter first number: " ))
+B=(input("Enter second number: "))
+
+print(A+B)      #prints in string format doesn't add the two variables
+
+print(int(A)+int(B))  #coverts the string formatted numbers to integer and prints all the arthimetic operations
+print(int(A)-int(B))
+print(int(A)*int(B))
+print(int(A)/int(B))
+print(int(A)%int(B))
