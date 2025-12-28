80e4c8d7c12727ee7a6d90409d4f300d1fc5c4ba
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-28 13:06:48 +0530

    Create Greater number.py
    works perfectly, made by using if, elif conditions.

---
+++ Greater number.py
@@ -0,0 +1,11 @@
+#program that shows which number is greater by seeing theinput given by the user 
+
+A=int(input("Enter first value: "))
+B=int(input("Enter second value: "))
+if A>B:
+    print(A,"Greater than B value")
+if A==B:
+    print(A,B,"Both are same values")
+elif B>=A:
+    print(B,"Greater than A value")
+
