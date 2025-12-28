659756deafdadd5ca37f77190dd2be3ed42c9cd9
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-28 12:51:19 +0530

    Create even or odd number.py
    works perfectly and by using if, else condition.

---
+++ even or odd number.py
@@ -0,0 +1,7 @@
+#program that shows whether the given inputis even or odd number
+
+num=int(input("Enter any number: "))
+if num %2==0:
+    print(num,"Even number")
+else :
+    print(num,"Odd number")
