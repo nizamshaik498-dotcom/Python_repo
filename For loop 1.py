8b9b3bea897fe164a0e39c0b6ca0b952cd564a5e
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-29 17:58:54 +0530

    Create For loop 1.py
    Made it for loop takes input from user and prints it product

---
+++ For loop 1.py
@@ -0,0 +1,6 @@
+#program using for loop and takes input from user and prints the multiplication of the table from 1 to 10
+
+Num=int(input("Enter any value: "))
+for i in range(1, 11):
+    print(f"{Num} x {i} = {Num*i}")
+    
