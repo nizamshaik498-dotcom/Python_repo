f9979016d03a77073ccd7e222891c55e74a37723
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-29 17:44:55 +0530

    Create while loop 2.py

---
+++ while loop 2.py
@@ -0,0 +1,7 @@
+#program using while loop by taking input from user and decementing until 1 occurs
+
+Value=int(input("Enter any value: "))
+while(Value >0):
+    print(Value)
+    Value=Value-1
+print("End of loop")
