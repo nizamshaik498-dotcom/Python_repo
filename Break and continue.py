352e2a6770984c28ee9700055e1baa15c40ded2a
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-30 22:46:12 +0530

    Update Break and continue.py

--- Break and continue.py
+++ Break and continue.py
@@ -3,9 +3,9 @@ while True:
     i=int(input("Enter any num:"))
     if i<0:
         print("skipped negative value")
-        continue
+        continue        #used continue here to skip
     elif i==0:
         print("stopped loop you entered zero")
-        break
+        break           #used break here to stoop the loop
     else:
         print(i,"Entered positive number")    
