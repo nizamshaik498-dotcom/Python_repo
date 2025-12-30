a1ac0e7115046be7dbcb98a6ac97a2ee22da7d12
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-30 23:03:27 +0530

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,11 +1,11 @@
 #program to check while the given input is divisible by 3 and 5 and prints the output
 
 check_num=int(input("Enter any num:"))
-if check_num %3==0 and check_num %5==0:
+if check_num %3==0 and check_num %5==0:     #Prints when divisable by both
     print(check_num,"fizzbuzz")
-elif check_num %3==0:
+elif check_num %3==0:                       #Prints only when its divisable by 3
     print(check_num,"fizz")
-elif check_num %5==0:
+elif check_num %5==0:                       #Prints only when its divisable by 5
     print(check_num,"buzz")
-else:
+else:                                       #Prints when not divisable by both
     print("Not divisable by both")
