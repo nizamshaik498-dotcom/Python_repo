1f9b5b2c610fd29f4a81d36f3fa5ae8efe67607b
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2025-12-31 19:29:21 +0530

    Create Functions 1.py
    Made by using function and also takes input from the user and prints the length and breadth of area and , and tell the area of rectangle

---
+++ Functions 1.py
@@ -0,0 +1,11 @@
+#program using function that asks input from the user and prints the length and breadth of a rectangle and also use return in it and prints the result
+
+def calculate_area():
+    length = float(input("Enter the length of the rectangle: "))
+    breadth = float(input("Enter the breadth of the rectangle: "))
+    return length, breadth
+
+length, breadth = calculate_area()
+print(f"The length of the rectangle is: {length}")
+print(f"The breadth of the rectangle is: {breadth}")
+print(f"The area of the rectangle is: {length * breadth}")
