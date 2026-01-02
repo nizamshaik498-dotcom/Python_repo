dc03202fcf41f071072249ed4595871f0f4a6773
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-02 19:39:37 +0530

    Create Tuple methods.py
    Made by using tuple methods and with f-string to print the output

---
+++ Tuple methods.py
@@ -0,0 +1,7 @@
+#program using tuple methods and f-string
+
+nums=(10,20,30,20,40,20,50)
+t=nums.count(20)
+t1=nums.index(20)
+print(f"20 appeared {t}times.")
+print(f"first occurence of 20 is:{t1}")
