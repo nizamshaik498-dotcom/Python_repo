c8f79e00a19271ab3e518837c61d1f2f9d41853f
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-02 19:58:43 +0530

    Create F-strings.py
    Made by using f-strings and takes input from user and prints the output.

---
+++ F-strings.py
@@ -0,0 +1,9 @@
+#program created by using f-strings and take input from user and print the output
+
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+city=input("Enter the city name: ")
+college=input("Enter college name: ")
+cgpa=float(input("Enter your marks: "))
+
+print(f"Hey my name is {name},and iam {age} years old.and i am currently staying in {city},studying in {college} college,and i got {cgpa} in my semesters")
