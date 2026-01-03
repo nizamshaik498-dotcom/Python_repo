139190cd5faa790e18fb7727a4925c3bdb86a4cd
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-03 13:42:55 +0530

    Update Recursion sum.py

--- Recursion sum.py
+++ Recursion sum.py
@@ -1,11 +1,11 @@
 #program made by function recursion
 
 def sum(n):
-    '''Takes the input from the user and prints the sum of N natural numbers'''
+    '''Takes the input from the user and prints the sum of N natural numbers'''     #docstring
     if(n==0):
         return 0
     else:
-        return n+sum(n-1)
+        return n+sum(n-1)               #recursive call
 n1=int(input("Enter any value: "))
 print(sum(n1))
-print(sum.__doc__)
+print(sum.__doc__)                #printing docstring   
