b88c3b0ef77e21614650732b1bb8caccb7a73b74
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-03 13:24:54 +0530

    Update Factorial_num.py

--- Factorial_num.py
+++ Factorial_num.py
@@ -1,11 +1,12 @@
 #program created by using recursion and docstring to calculate factorial of an number
 
 def factorial(n):
-    ''' Takes an input value from the user and prints its factorial'''
+    ''' Takes an input value from the user and prints its factorial'''      #docstring
     if(n==0 or n==1):
         return 1
     else:
-        return n*factorial(n-1)
+        return n*factorial(n-1)         #recursive call
 n1=int(input("enter any value: "))
 print(factorial(n1))
-print(factorial.__doc__)
+print(factorial.__doc__)                #printing docstring
+
