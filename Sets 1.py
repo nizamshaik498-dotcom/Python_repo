bd19d2d0f154d9c31747d87956d6cd0501b10493
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-04 18:58:19 +0530

    Update Sets 1.py

--- Sets 1.py
+++ Sets 1.py
@@ -1,7 +1,7 @@
 #program made by using list 
 id_list={101,102,103,104}
 id_no=int(input("Enter your id number: "))
-if (id_no in id_list):
+if (id_no in id_list):                          #used in operator to check whether the id number is present in the set or not
     print("Access Granted")
 else:
     print("Access denied")
