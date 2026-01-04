c0dfe1fe29f6d416825010dda770f76705b84abf
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-04 18:57:14 +0530

    Create Sets 1.py
    Made by using set and if ,else condition
    to check whether the id is present in the set if present it prints access granted if not then prints access denied

---
+++ Sets 1.py
@@ -0,0 +1,7 @@
+#program made by using list 
+id_list={101,102,103,104}
+id_no=int(input("Enter your id number: "))
+if (id_no in id_list):
+    print("Access Granted")
+else:
+    print("Access denied")
