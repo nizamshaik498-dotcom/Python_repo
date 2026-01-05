76f93d4aed5f52d2654ceda4e244b8e5a7e977d5
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-05 20:06:08 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,6 +1,6 @@
 #program created by using dictonary and printing the sum and average of the values in the dictionary present
 
-Marks={"English":95,
+Marks={"English":95,                #dictionary stored with values with its items
        "Sanskrit":92,
        "Maths":90,
        "Physics":72,
@@ -11,9 +11,10 @@ print(Marks["Maths"])
 print(Marks["Physics"])
 print(Marks["Computers"])
 
-add=(95+92+90+72+89)
+add=(95+92+90+72+89)                #Sum the total marks
 print("Total Marks: ",add)
-avg=(95+92+90+72+89/5)
+avg=(95+92+90+72+89/5)              #Average of the marks
 print("Average: ",avg)
 
+print(type(Marks))                  #prints the type of the variable Marks
 
