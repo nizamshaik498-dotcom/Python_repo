5986f92b8f52a3d3da24f8b1e1deba72e10e64fb
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-06 20:44:28 +0530

    Update Exception handling.py

--- Exception handling.py
+++ Exception handling.py
@@ -1,10 +1,10 @@
 #program created by using exception handling statements like (try,except)
 
-try:
+try:                                                    #used try  block
     a=int(input("Enter 1st value: "))
     b=int(input("Enter 2nd value: "))
     print(a/b)
-except ZeroDivisionError:
+except ZeroDivisionError:                                  #used except block
     print("Cannot divide by zero")
 except ValueError:
     print("invalid input")
