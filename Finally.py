f42dc53d9b11682764f612524b8be11589969607
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-07 19:52:18 +0530

    Update Finally.py

--- Finally.py
+++ Finally.py
@@ -6,7 +6,7 @@ try:                                                        #used try block to t
     num1=int(input("Enter first value: "))
     num2=int(input("Enter second value: "))
     result=(num1/num2)
-    print("Your answer is:",result)
+    print("Your answer is:",result)                        #only this line will execute if there is no exception
 except ZeroDivisionError:                                  #used except block to handle the exception   
     print("Sorry! cant divide with zero")
 except ValueError:                                         #used another except block to handle another exception
