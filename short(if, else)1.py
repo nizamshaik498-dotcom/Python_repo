1e029287e0177b745d544e514c61959bc29f90fa
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-09 20:26:00 +0530

    Create short(if, else)1.py
    Made with using short hand if else statements and takes input from the user and prints if he is eligible to vote or not

---
+++ short(if, else)1.py
@@ -0,0 +1,4 @@
+#program made using short hand (if , else) statements and takes input from user and prints he is eligible for vote
+
+age=int(input("Enter your age: "))
+print("Eligible for vote")if age>=18 else print("Not eligible for vote")
