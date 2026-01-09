050f13c64ff9badd39671046a47e969279f87d9c
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-09 20:27:36 +0530

    Update short(if, else).py

--- short(if, else).py
+++ short(if, else).py
@@ -1,4 +1,4 @@
 #program made using short hand (if , else) statements takes input from user and prints whether it is even or odd num
 
 num=int(input("Enter any number: "))
-print(num,"Even number")if num%2==0 else print(num,"odd number")
+print(num,"Even number")if num%2==0 else print(num,"odd number")        #short hand if else statement
