fc092d17a7fc368d1f610dd06f035a9befec8acb
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-09 20:30:25 +0530

    Update short(if, else)1.py

--- short(if, else)1.py
+++ short(if, else)1.py
@@ -1,4 +1,4 @@
 #program made using short hand (if , else) statements and takes input from user and prints he is eligible for vote
 
-age=int(input("Enter your age: "))
+age=int(input("Enter your age: "))                                                      #taking input from user
 print(age,"Eligible for vote")if age>=18 else print(age,"Not eligible for vote")        #short hand if else statement
