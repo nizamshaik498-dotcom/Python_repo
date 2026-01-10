4ba4260a797457fca33575a6708060e6499244f6
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-10 19:21:28 +0530

    Update Enumarate function.py

--- Enumarate function.py
+++ Enumarate function.py
@@ -3,3 +3,7 @@
 Fruits=["Apple","Mango","Guava","Pine apple","Orange","Melon","Kiwi"]
 for index,value in enumerate(Fruits ,start=1):
     print(index,value)
+#  Another way to write the same code
+print("Another way to write the same code")
+for index,value in enumerate(Fruits ,start=1):
+    print(index,value)
