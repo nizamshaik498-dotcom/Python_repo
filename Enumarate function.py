8923c50dd9c3803ab48c0196d7e6040d18a1f609
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-10 19:25:47 +0530

    Update Enumarate function.py

--- Enumarate function.py
+++ Enumarate function.py
@@ -1,7 +1,7 @@
 #Program created using enumarate function which shows both index number and value stored in the list
 
 Fruits=["Apple","Mango","Guava","Pine apple","Orange","Melon","Kiwi"]               #used an list of fruits
-for index,value in enumerate(Fruits ,start=1):                                      #used enumarate function to get index number and value
+for index,value in enumerate(Fruits ,start=1):                                      #prints index number starting from 1
     print(index,value)
 #  Another way to write the same code
 print("Another way to write the same code")
