bd4178ef69bd064b3364ab75bc43a8564afbcce1
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-13 20:52:21 +0530

    Update For loop4.py

--- For loop4.py
+++ For loop4.py
@@ -1,7 +1,7 @@
 #program created using for loop which prints number which are greater and less than 10.
 
 num=[2,5,8,11,14]
-for value in num:
+for value in num:                               #used for loop to iterate through the list
     if value>10:
         print(value,"is greater than 10")
     else:
