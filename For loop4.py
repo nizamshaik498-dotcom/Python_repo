d583bc7066eacbb0508095cb540a06689939173d
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-13 20:51:45 +0530

    Create For loop4.py
    Made using list and used for loop  and as well as if, else conditions to print whether the numbers in the list are greater or not

---
+++ For loop4.py
@@ -0,0 +1,8 @@
+#program created using for loop which prints number which are greater and less than 10.
+
+num=[2,5,8,11,14]
+for value in num:
+    if value>10:
+        print(value,"is greater than 10")
+    else:
+        print(value,"not greater than 10")
