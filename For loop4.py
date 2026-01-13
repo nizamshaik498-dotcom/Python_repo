2c17aac20f5ab8e8f612dd76275b5e38ffd7c31e
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-13 20:52:57 +0530

    Update For loop4.py

--- For loop4.py
+++ For loop4.py
@@ -2,7 +2,7 @@
 
 num=[2,5,8,11,14]
 for value in num:                               #used for loop to iterate through the list
-    if value>10:
+    if value>10:                                #used if else condition to check whether the number is greater than 10 or not
         print(value,"is greater than 10")
     else:
         print(value,"not greater than 10")
