db56e553dae39c85a72bf544df044c0add0dc4e7
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-15 20:22:02 +0530

    Create ATM.py
    Made an ATM system using if, else, elif conditions

---
+++ ATM.py
@@ -0,0 +1,28 @@
+#program created using if, else and elif conditions . (ATM mini system)
+
+pin=1234
+balance=5000
+
+atm_pin=int(input("Enter ATM pin: "))
+if atm_pin!=pin:
+    print("Incorrect PIN")
+else:
+    print("1. Check Balance")
+    print("2. Withdraw Money")
+    print("3. Exit")
+
+    choice=int(input("Choose any option: "))
+    if choice==1:
+        print("Your available balance: ",balance)
+    elif choice==2:
+        amount=int(input("Enter Amount: "))
+        if amount>balance:
+            print("Insufficient Balance")
+        else:
+            balance=balance-amount
+            print("Remaining balance: ",balance)
+    elif choice==3:
+        print("Thank you, visit again")
+    else:
+
+        print("Invalid choice")    
