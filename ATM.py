5e1030a772a80e38c1dbe00f71f8359ac65692c8
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-15 20:24:21 +0530

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,7 +1,7 @@
 #program created using if, else and elif conditions . (ATM mini system)
 
 pin=1234                                  #predefined pin
-balance=5000
+balance=5000                          #predefined balance
 
 atm_pin=int(input("Enter ATM pin: "))                           #takes input from user
 if atm_pin!=pin:
