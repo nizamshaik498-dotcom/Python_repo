11ddb9f69357aad0b826edbee778814303c4d427
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-17 20:18:04 +0530

    Update Phone Lock.py

--- Phone Lock.py
+++ Phone Lock.py
@@ -3,7 +3,7 @@
 password=4321
 attempts=3
 
-while attempts>0:
+while attempts>0:                                       #used while loop to allow multiple attempts
     pin=int(input("Enter PIN Number: "))
     if pin==password:
         print("Phone Unlocked")
