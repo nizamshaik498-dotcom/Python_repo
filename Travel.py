9c7cea9f6b79d072349b2687a13fb6533e9bc4dc
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-20 19:59:13 +0530

    Create Travel.py
    Made by using if, elif and else statements. And also f-string in this, which gives amount and also confirms the user to book or cancel the ride, price depends on distance.

---
+++ Travel.py
@@ -0,0 +1,35 @@
+#program created using if, else, elif .
+
+print("Uber Cab Services")
+name=input("Enter Your Name: ")
+address=input("Enter city name you want to travel: ")
+distance=int(input("Enter km: "))
+book_ride=int(input("Confirm to book your ride enter (1): "))
+if book_ride==1:
+    print("Ride is booked")
+elif book_ride==1 and distance<=0:
+    print("Sorry cant book ride!!! Distance is too low")
+else:
+    print("Ride cancelled")
+if distance<=0:
+    print(" Distance must be greater than (0) km")
+elif distance<=100:
+    print(f"100 rupees for going to {address}")
+    print(f"Enjoy the ride {name}")
+elif distance>100 and distance<=200:
+    print(f"250 rupees for going to {address}")
+    print(f"Enjoy the ride {name}")
+elif distance>200 and distance<=500:
+    print(f"350 rupees for going to {address}")
+    print(f"Enjoy the ride {name}")
+elif distance>500 and distance<=800:
+    print(f"500 rupees for going to {address}")
+    print(f"Enjoy the ride {name}")
+elif distance>800 and distance<=1000:
+    print(f"Between 850 to 1000 for going to {address}")
+    print(f"Enjoy the ride {name}")
+else:
+    print(f"Thanks for trying our services {name}")
+    print("Sorry but our cab services ranges between 100 to 1000 km only")
+
+
