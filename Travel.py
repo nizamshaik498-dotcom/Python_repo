0e2a78287444edb71ecba2b7c01c15c9c57f486b
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-20 20:02:51 +0530

    Update Travel.py

--- Travel.py
+++ Travel.py
@@ -3,7 +3,8 @@
 print("Uber Cab Services")
 name=input("Enter Your Name: ")
 address=input("Enter city name you want to travel: ")
-distance=int(input("Enter km: "))
+distance=int(input("Enter km: "))                             #tasking distance from user
+print(f"Hello {name}, you want to travel to {address} which is {distance} km away")
 book_ride=int(input("Confirm to book your ride enter (1): ")) #1 for booking ride
 if book_ride==1:
     print("Ride is booked")
