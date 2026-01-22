fd3a1938bd643cd1fa08f5d60672262c8bb1cfaf
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-22 21:16:10 +0530

    Create Movie Ticket Booking.py
    Made by using if, elif, else statements
    which takes input from the user and gives the bill for the movie

---
+++ Movie Ticket Booking.py
@@ -0,0 +1,30 @@
+#program created using if, elif, else statements and by using arthimetic operators.
+
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+tickets=int(input("Enter number of tickets: "))
+
+if age<12:
+    price_per_ticket=100
+elif age<=60:
+    price_per_ticket=200
+else:
+    price_per_ticket=150
+
+total_amount=price_per_ticket*tickets
+
+if tickets>5:
+    discount=total_amount*0.10
+else:
+    dicount=0
+
+final_amount=total_amount-discount
+
+print("-----Movie Ticket Bill-----")
+print("Customer Name: ",name)
+print("Ticket Price: ",price_per_ticket)
+print("Number Of tickets: ",tickets)
+print("total Amount: ",total_amount)
+print("Discount: ",discount)
+print("Final Amount To Pay: ",final_amount)
+print("-----------------------------")
