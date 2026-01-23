e56cd9abac7172c4bbc543c7720816f7c2016d61
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-23 21:03:15 +0530

    Create Mini grocery store.py
    Made an mini fruit shop type program using dictionary which takes fruit name as input and prints the price of it based on the quantity purchased

---
+++ Mini grocery store.py
@@ -0,0 +1,10 @@
+#program created using dictionary and made mini grocery store.
+
+print("-----The Fruit Land-----")
+item_price={"apple":50,"mango":100,"watermelon":80,"kiwi":250,"guava":30,"avocado":500}
+item=input("Enter fruit name: ")
+quantity=int(input("Enter quantity of fruits: "))
+if item in item_price:
+    print(item_price[item]*quantity,"Total price")
+else:
+    print("Not available in store!!!")
