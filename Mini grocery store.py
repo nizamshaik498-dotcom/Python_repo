bac30e5f631d0bf14bc73d87c996d72b00e5938e
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-23 21:04:28 +0530

    Update Mini grocery store.py

--- Mini grocery store.py
+++ Mini grocery store.py
@@ -5,6 +5,6 @@ item_price={"apple":50,"mango":100,"watermelon":80,"kiwi":250,"guava":30,"avocad
 item=input("Enter fruit name: ")
 quantity=int(input("Enter quantity of fruits: "))       #takes input from user for item and quantity
 if item in item_price:
-    print(item_price[item]*quantity,"Total price")
+    print(item_price[item]*quantity,"Total price")      #calculates total price for the item
 else:
     print("Not available in store!!!")
