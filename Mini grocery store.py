e60c1f5457ba24dbf9d83f5503e0ca84f2cb83c9
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-23 21:04:56 +0530

    Update Mini grocery store.py

--- Mini grocery store.py
+++ Mini grocery store.py
@@ -2,7 +2,7 @@
 
 print("-----The Fruit Land-----")
 item_price={"apple":50,"mango":100,"watermelon":80,"kiwi":250,"guava":30,"avocado":500}         #used dictionary to store item and price
-item=input("Enter fruit name: ")
+item=input("Enter fruit name: ")                        #takes input from user for item
 quantity=int(input("Enter quantity of fruits: "))       #takes input from user for item and quantity
 if item in item_price:
     print(item_price[item]*quantity,"Total price")      #calculates total price for the item
