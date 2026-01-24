b8d51e679c7a9919d3cfac53a2361c45b3c63a89
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-24 23:07:01 +0530

    Update Password strength checker.py

--- Password strength checker.py
+++ Password strength checker.py
@@ -1,8 +1,7 @@
 #program created using if, else, elif and string operations which checks the password strength entered by the user.
 
 key=input("Enter Password: ")                                           #takes password input in both numbers and letters
-length=6
-if len(key)<6:
+if len(key)<6:                                                          #checks the length of password
     print("Weak Password")
 elif key.isdigit():
     print("Password should contain numbers and as well as letters")
