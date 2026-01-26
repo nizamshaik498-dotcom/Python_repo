a073868a7d69b4bec9941010643f3159b05a049a
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-26 21:27:47 +0530

    Update Function squares.py

--- Function squares.py
+++ Function squares.py
@@ -1,7 +1,7 @@
 #program created using functions and prints the square value from the numbers given as input from the user.
 
 val1=int(input("Enter 1st number: ")) #input from user
-val2=int(input("Enter 2nd number: "))
+val2=int(input("Enter 2nd number: ")) #input from user
 def value():
     num=val1*val1+val2*val2
     return num
