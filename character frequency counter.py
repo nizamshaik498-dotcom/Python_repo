78c39a47cbb86b831aeebe2af0ae5ff0b94e6732
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-28 20:36:06 +0530

    Create character frequency counter.py
    Made a program using string methods which shows how many lower, upper, digits, spaces available in the input  given by the user.

---
+++ character frequency counter.py
@@ -0,0 +1,21 @@
+#program created using string methods .(character frequency counter).
+
+query=input("Enter any sentence: ")
+lower=0
+upper=0
+digit=0
+space=0
+for n in query:
+    if n.islower():
+        lower+=1
+    elif n.isupper():
+        upper+=1
+    elif n.isdigit():
+        digit+=1
+    elif n.isspace():
+        space+=1
+    print("Lowercase Letters: ",lower)
+    print("Uppercase letters: ",upper)
+    print("Digits: ",digit)
+    print("Spaces: ",space)
+
