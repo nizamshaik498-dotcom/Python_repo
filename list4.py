07f7ea8813b3afeb393c73fd38052f74dc41990d
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-29 21:02:40 +0530

    Update list4.py

--- list4.py
+++ list4.py
@@ -1,7 +1,7 @@
 #program created using list which stores the values given by user in it and shows largest, smallest and average of the values
 
 num=list(map(int,input("Enter numbers: ").split())) #takes input from user and store in the list
-largest=max(num)
+largest=max(num)                                    #finds the largest number in the list
 smallest=min(num)
 average=sum(num)/len(num)
 print("The largest number is: ",largest)
