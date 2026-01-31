3ab05b69162d5f4a42aa9a3ad5e00a5d20e2a30a
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-01-31 21:05:57 +0530

    Update Dictionary 2.py

--- Dictionary 2.py
+++ Dictionary 2.py
@@ -1,7 +1,7 @@
 #program created using dictionary to store contacts
 name=input("Enter name: ")                                                          #input name to search in contacts
 contacts={"nizam":154251,"althaf":567423,"wahid":654786,"mike":367399}              #dictionary to store contacts
-name=name.lower()
+name=name.lower()                                                                   #convert input name to lowercase    
 if name in contacts:
     print("The mobile number is: ",contacts[name])
 else:
