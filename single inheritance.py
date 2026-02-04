73f2b8ee8d359a01e3d27459654e4106504aae10
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-04 21:39:35 +0530

    Update single inheritance.py

--- single inheritance.py
+++ single inheritance.py
@@ -1,6 +1,6 @@
 #python program created using single inheritance.
 class Animal:                           #class Animal
-    def speak(self):                   
+    def speak(self):                    #method speak of class Animal
         print("Animal Sound")
 class Dog(Animal):                      #class Dog inherited from class Animal           
     def bark(self):
