08085609fb5fd5f341272c375cf7168440c130d6
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-05 20:50:42 +0530

    Update multilevel inheritance.py

--- multilevel inheritance.py
+++ multilevel inheritance.py
@@ -3,7 +3,7 @@
 class Animal:                                   #base class
     def speak(self):                            #method of base class
         print("Animal Sound")
-class Dog(Animal):                                 
+class Dog(Animal):                              #derived class inheriting from base class
     def bark(self):                     
         print("Barking sound")
 d=Dog()
