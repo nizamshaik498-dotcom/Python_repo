96d07fbd7b1ab757c641590d444983db34c963e6
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-06 23:12:19 +0530

    Update Hybrid inheritance.py

--- Hybrid inheritance.py
+++ Hybrid inheritance.py
@@ -3,7 +3,7 @@
 class ceo:                                          #base class
     def ceomethod(self):                            #method of ceo class
         print(("I am the ceo"))
-class manager(ceo):
+class manager(ceo):                                 #derived class of ceo class
     def managermethod(self):
         print("I am manager")
 class employee1(manager,ceo):
