1b5b7c0e3b5f6fda5aaed4a8684335769c5e4180
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-07 20:45:05 +0530

    Create Polymorphism.py
    Made a python program using class polymorphism

---
+++ Polymorphism.py
@@ -0,0 +1,17 @@
+#Program created using class polymorphism
+
+class BMW:
+    def car(self):
+        print("Byrische Motoren Worke")
+    def model(self):
+        print("M5")
+class Lamborghini:
+    def car(self):
+        print("The bull of cars")
+    def model(self):
+        print("Aventador")
+ob=BMW()
+ob1=Lamborghini()
+for vehicles in(ob,ob1):
+    vehicles.car()
+    vehicles.model()
