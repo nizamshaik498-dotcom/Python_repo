8b7e6e454811886bbce4f8c8250d814a155c741c
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-08 20:49:21 +0530

    Create constructor2.py
    Made a program using parameterized constructor which shows the employee data.

---
+++ constructor2.py
@@ -0,0 +1,17 @@
+#program using parameterized constructor.(Employee Data)
+
+print("***_____Employee Data_____***")
+class Employee:
+    def __init__(self,empid,empname,salary,department):
+        self.empid=empid
+        self.empname=empname
+        self.salary=salary
+        self.department=department
+    def display(self):
+        print("Employee ID: ",self.empid)
+        print("Employee Name: ",self.empname)
+        print("Employee Salary: ",self.salary)
+emp1=Employee(101,"akshay",50000,"HR dept")
+emp2=Employee(102,"Sanjay",65000,"Finance dept")
+emp1.display()
+emp2.display()
