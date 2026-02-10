c2c7cb32de54028befd1ed1ca6d45dd3d88db0d4
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-10 20:41:13 +0530

    Update Bank management system.py

--- Bank management system.py
+++ Bank management system.py
@@ -13,7 +13,7 @@ class BankAccount:                                                      #bank ac
         self.balance=self.balance+amount
         print("Amount Deposited: ",amount)
     def withdraw(self,amount):                                          #method created to withdraw the amount from the account
-        if amount<=self.balance:
+        if amount<=self.balance:                                        #condition created to check whether the amount to be withdrawn is less than or equal to the balance in the account
             self.balance=self.balance-amount
             print("Amount Withdrawn: ",amount)
         else:
