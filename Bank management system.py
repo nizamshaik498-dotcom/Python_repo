f12c962bf645bfcf7d983bebc592af95de9db343
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-10 20:38:07 +0530

    Create Bank management system.py
    Made a program using oop concept for bank management system, which shows amount deposited , withdrawn, and  available balance.

---
+++ Bank management system.py
@@ -0,0 +1,24 @@
+#program created using (oop) based for bank.
+
+class BankAccount:
+    def __init__(self,acname,acnum,balance):
+        self.acname=acname
+        self.acnum=acnum
+        self.balance=balance
+    def display(self):
+        print("account holder name: ",self.acname)
+        print("account number: ",self.acnum)
+        print("account balance: ",self.balance)
+    def deposit(self,amount):
+        self.balance=self.balance+amount
+        print("Amount Deposited: ",amount)
+    def withdraw(self,amount):
+        if amount<=self.balance:
+            self.balance=self.balance-amount
+            print("Amount Withdrawn: ",amount)
+        else:
+            print("Insufficient Balance")
+d=BankAccount("Alex",501250,50000)
+d.deposit(5000)
+d.withdraw(12000)
+d.display()
