2f97fd7e524fddaa6f4cc95a9549306775e7f1b2
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-11 21:15:36 +0530

    Create Server resource monitor.py
    Made a program for  server resource monitor using if else conditions.

---
+++ Server resource monitor.py
@@ -0,0 +1,20 @@
+#program created for (server resource monitor).
+cpu_capacity=input("Enter total CPU capacity: ")
+cpu_capacity=float(cpu_capacity.replace("%",""))
+cpu_usage=input("Enter CPU usage: ")
+cpu_usage=float(cpu_usage.replace("%",""))
+total_memory=input("Enter total memory in GB: ")
+total_memory=float(total_memory.replace("GB",""))
+used_memory=input("Enter used memory in GB: ")
+used_memory=float(used_memory.replace("GB",""))
+
+rem_cpu=cpu_capacity-cpu_usage
+print("Remaining CPU: ",rem_cpu)
+
+rem_mem=total_memory-used_memory
+print("Remaining Memory: ",rem_mem)
+
+if cpu_usage >=80 or used_memory>=16:
+    print("Server Under High Load ⚠️")
+else:
+    print("Server Running Normally 👌")
