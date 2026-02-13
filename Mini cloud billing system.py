1eec025b6612f72b3231e9dfeae102495b090fb3
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-13 20:01:23 +0530

    Update Mini cloud billing system.py

--- Mini cloud billing system.py
+++ Mini cloud billing system.py
@@ -6,7 +6,7 @@ inst_cost=float(input("Enter cost per hour of instance: "))                 #ent
 s3_storage=int(input("Enter the s3 storage in GB: "))                       #entering s3 storage in GB
 s3_cost=2
 
-ec2_total=instances*inst_hours*inst_cost                                
+ec2_total=instances*inst_hours*inst_cost                                    #calculating total cost for ec2 instances
 s3_total=s3_storage*s3_cost
 total_bill=ec2_total*s3_total
 
