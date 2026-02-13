575b689bcc459bd900bb74235499ee604d8e9272
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-02-13 20:42:35 +0530

    Update Number Guesser.py

--- Number Guesser.py
+++ Number Guesser.py
@@ -4,7 +4,7 @@ num=57
 attempts=5                                                                      #assigning the number of attempts and the number to be guessed.
 
 while attempts>0:                                                               #using while loop to check the number of attempts and to take input from user until the attempts are over or the user wins.
-    number=int(input("Enter any random number: "))
+    number=int(input("Enter any random number: "))                              #taking input from user and converting it into integer.
     if number==num:
         print("***You won***")
         break
