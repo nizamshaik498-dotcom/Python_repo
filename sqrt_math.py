c2f018e7f94d4067ad5a655a10699e9962ccca08
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-05-12 16:17:56 +0530

    Update sqrt_math.py

--- sqrt_math.py
+++ sqrt_math.py
@@ -1,3 +1,3 @@
 #created a program which prints the square root of any number
 import math
-print(math.sqrt(144))
+print(math.sqrt()) #enter the number you want to find the square root of in the parentheses
