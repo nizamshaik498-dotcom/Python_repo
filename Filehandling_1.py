331ab0580b8a0700ad8bea0510b10da179c23877
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-05-14 17:12:16 +0530

    Update Filehandling_1.py

--- Filehandling_1.py
+++ Filehandling_1.py
@@ -4,7 +4,8 @@ with open ("notes.txt","w") as file:
     file.write("Used writing method here")
 
 with open ("notes.txt","r") as file:
-    fileread=file.read()
+    print(file.read())
 
 with open("notes.txt","a") as file:
     file.write("\nUsed append method here")
+
