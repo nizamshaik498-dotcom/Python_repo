f63cea613d2cbd92075f5df3d47b76b2dad85113
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-05-16 16:47:09 +0530

    Added greetings python files

---
+++ greetings.py
@@ -0,0 +1,6 @@
+#Created a program using functions which do greetings (module concept)
+
+def hello():
+    print("Good morning")
+def bye():
+    print("Bye have a nice day")
---
+++ main1.py
@@ -0,0 +1,6 @@
+#Created a program which function calls the functions from greetings.py file
+
+import greetings
+
+greetings.hello()
+greetings.bye()
